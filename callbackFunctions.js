802bb0d2723114562ec4cabc5a4611fc9da07786
Mohammadali Hamam <mhamam2@uwo.ca>
2023-10-03 08:25:43 -0400

    completiong of task 2

--- callbackFunctions.js
+++ callbackFunctions.js
@@ -1,20 +1,22 @@
-
-let triangleFrame = function(letter)
-{
+let triangleFrame = function(letter) {
     let framedLetter = '';
-    for (let i=0;i<4;i++)
-    {
-        let line='';
-        // write a loop to start the line with 3 - i spaces
-       
+    for (let i = 0; i < 4; i++) {
+        let line = '';
 
-        // The third line will contain the framed letter so we need to set the right condition
-        if (/*set the condition to specify the third line */) 
-            line += `* ${letter} *`;
-        else
-        {
-            // the rest of the lines of the triangle would contain all stars
-            // create a loop to add 2i + 1 stars to the line
+        // Loop to start the line with 3 - i spaces
+        for (let j = 0; j < 3 - i; j++) {
+            line += ' ';
+        }
+
+        // The third line will contain the framed letter
+        if (i === 2) {
+            line += `* ${letter} *`;  // Fix the string interpolation
+        } else {
+            // The rest of the lines of the triangle would contain all stars
+            // Create a loop to add 2i + 1 stars to the line
+            for (let k = 0; k < 2 * i + 1; k++) {
+                line += '*';
+            }
         }
 
         framedLetter += line + '\n';
@@ -23,33 +25,41 @@ let triangleFrame = function(letter)
     return framedLetter;
 }
 
+// Test the triangleFrame function
+console.log(triangleFrame('A'));
 
-let rectangleFrame = function(letter)
-{
-    framedLetter = '';
-    line = "";
+// Define the rectangleFrame function
+let rectangleFrame = function(letter) {
+    let framedLetter = '';
+    let line = '';
 
-    //write a loop the add five stars to the line
-   
+    // Loop to add five stars to the line
+    for (let i = 0; i < 5; i++) {
+        line += '*';
+    }
 
-    //set the framed letter to be of 3 lines (one line of star, 
-    //one line * Letter * and the last line of all stars)
+    // Set the framed letter to be of 3 lines (one line of stars, 
+    // one line * Letter * and the last line of all stars)
     framedLetter = line + '\n' + `* ${letter} *` + '\n' + line + '\n';
 
     return framedLetter;
 }
 
-
 // Set the function definition for the generalFramer so that it would receive two arguments
-//      - the first argument is a callback function that creates the desired frame
-//      - the second argument is the letter to be framed.
-// The retun should be the string of the letter framed by the given framer.
-let generalFramer;
+// - the first argument is a callback function that creates the desired frame
+// - the second argument is the letter to be framed.
+// The return should be the string of the letter framed by the given framer.
 
-console.log(generalFramer(/*set the argument to frame the letter A in a triangle*/));
-console.log(generalFramer(/*set the argument to frame the letter B in a triangle*/));
-console.log(generalFramer(/*set the argument to frame the letter C in a triangle*/));
+let generalFramer = function(framerCallback, letter) {
+    return framerCallback(letter);
+}
 
-console.log(generalFramer(/*set the argument to frame the letter A in a rectangle*/));
-console.log(generalFramer(/*set the argument to frame the letter B in a rectangle*/));
-console.log(generalFramer(/*set the argument to frame the letter C in a rectangle*/));
+// Test the generalFramer function with different framers and letters
+
+console.log(generalFramer(triangleFrame, 'A'));
+console.log(generalFramer(triangleFrame, 'B'));
+console.log(generalFramer(triangleFrame, 'C'));
+
+console.log(generalFramer(rectangleFrame, 'A'));
+console.log(generalFramer(rectangleFrame, 'B'));
+console.log(generalFramer(rectangleFrame, 'C'));
--- numberInspector.js
+++ numberInspector.js
@@ -12,9 +12,20 @@
 {
     switch (task)
     {
+        case "palindrome":
+            return palindrome;
+        case "max":
+            return max;
+        case "duplicate":
+            return duplicate;
+        case "divisibleBy":
+            return divisibleBy;
+        default:
+            return null;
+
         // Complete the code so that based on a given string "palindrome, max, duplicate, divisibleBy" 
         // The appropriate function is returned. 
-     
+
     }
 
      /*Write a function that returns the palindrome number of the number you are given, 
@@ -23,7 +34,21 @@
     *Keep in mind that you can access each digit of a number through simple operations. 
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a numberr*/
-    function 
+    function flipNumber(number){
+        let flip = 0;
+
+        while(number > 0){
+            const lastDigit = number % 10;
+            flip = flip * 10 + lastDigit;
+            number = Math.floor(number / 10);
+        }
+
+        return flip;
+    }
+    const inputNumber = 1234;
+    const palindromeNumber = flipNumber(inputNumber);
+    console.log(palindromeNumber); // Output: 4321
+
 
     /* Write a function that will return the maximum digit of the number you are given.
     * For example, if you are given 192345, it should return 9. If you are given 12645 it should return 6. 
@@ -31,7 +56,24 @@
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a numberr*/
 
-    function 
+    function locateMaxDigit(number){
+        let maxDigit = 0; 
+
+        while (number > 0){
+            const digit = number % 10;
+
+            if (digit > maxDigit){
+                maxDigit = digit;
+            }
+            number = Math.floor(number/10);
+        }
+
+       return maxDigit;
+    }
+
+    console.log(locateMaxDigit(192345));
+    console.log(locateMaxDigit(12645));
+
     /*This function can be use to identify whether a number contains 2 or more consecutive duplicate numbers
     * For example, if you are given 1223 it should return yes because we have 22 in our number. Similarly, 
     * 12344 should also return true while 23452 should return false
@@ -39,7 +81,22 @@
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a number, the remainder operator is also helful*/
      
-    function 
+    function consecutiveDuplicateIndicator(number){
+        let lastDigit = -1;
+
+        while (number > 0){
+            const currentDigit = number % 10;
+
+            if (currentDigit === lastDigit){
+                return true;
+            }
+
+            lastDigit = currentDigit;
+            number = Math.floor(number/10);
+        }
+
+        return false;
+    }
     /*This function will return the value needed to be added to a number to make it divisible by the given dividor. 
     *For example, if you are given 63 and 5, we are looking for the number needed to make 63 divisible by 5. In this case, 
     the function should return 2. To challenged yourself, try to use the && or || operator.
@@ -47,7 +104,16 @@
     *You can assume that this function will receive a number as a parameter. 
     *HINT: You can use Math.floor to get the integer closest to a number, the remainder operator is also helful*/
 
-    function 
+    function makeItDivisible(number, divsior){
+
+        const remainder = number % divisor;
+
+        const neededValue = (divisor - remainder) % divisor;
+
+        return neededValue;
+    }
+
+    console.log(makeItDivisible(63,5));
 
 }
 
